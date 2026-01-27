da8bce4edf41e6e86e16bad29e2a305281252430
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-01-27 15:48:48 +0530

    register logic and test on postman

---
+++ prysm-node-backend/Controller/auth.controller.js
@@ -0,0 +1,40 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
+
+const register = async(req,res)=>{
+    try{
+    const {name , email, password , role} = req.body;
+
+    const existingUser = await prisma.user.findUnique({
+        where:{email}
+    });
+
+    if(existingUser){
+        return res.status(409).json({message:"Email already exists"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password , 10);
+
+    const user = await prisma.user.create({
+        data:{
+            name,
+            email,
+            password:hashedPassword,
+            role
+        }
+    });
+    res.status(201).json({
+        id:user.id,
+        name:user.name,
+        email:user.email,
+        role:user.role
+    })
+    }
+    catch(error){
+      console.error(error);
+      res.status(500).json({message:"Server error"});
+    }
+}
+
+module.exports = {register};
--- prysm-node-backend/app.js
+++ prysm-node-backend/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const PORT  = 5000;
+const authRouter = require('./router/auth.routes');
+app.use(express.json());
+app.use('/auth', authRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server is runing on ${PORT}`)
