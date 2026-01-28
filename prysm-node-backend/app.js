0c45f206e600d60a8413721d6192cf87efaa873f
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-01-28 08:13:09 +0530

    change the port number for deploy the application on vps

--- prysm-node-backend/app.js
+++ prysm-node-backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
-const PORT  = 5000;
+const PORT  = 5002;
 app.use(express.json());
 const authRouter = require('./router/auth.routes');
 const userRouter = require("./router/user.router");
